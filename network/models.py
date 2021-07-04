cc9fbe4e6dc24237d802a8fbb21e49b95fe72816
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-07-04 18:23:53 +0200

    add automatic resizing of avatars on upload

--- network/models.py
+++ network/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from sorl.thumbnail import ImageField, get_thumbnail
+from sorl.thumbnail import ImageField
+from PIL import Image, ImageOps, ExifTags
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
 
 
 class User(AbstractUser):
@@ -32,14 +36,45 @@ class User(AbstractUser):
         verbose_name = 'User'
         verbose_name_plural = 'Users'
 
-    # TODO figure out how to make this work
-    # def save(self, *args, **kwargs):
-    #     if self.avatar:
-    #         self.avatar = get_thumbnail(self.avatar, '200x200', quality=80, format='JPEG')
-    #     super(User, self).save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        img = Image.open(self.avatar)
+        if img.height > 200 or img.width > 200:
+            output_size = (200, 200)
+            img = ImageOps.fit(img, output_size, Image.ANTIALIAS)
+            img = img.convert('RGB')
+
+            # rotate image correctly based on exif data
+            try:
+                for orientation in ExifTags.TAGS.keys():
+                    if ExifTags.TAGS[orientation]=='Orientation':
+                        break
+                exif = img.getexif()
+                if exif[orientation] == 3:
+                    img = img.rotate(180, expand=True)
+                elif exif[orientation] == 6:
+                    img = img.rotate(270, expand=True)
+                elif exif[orientation] == 8:
+                    img = img.rotate(90, expand=True)
+            except (AttributeError, KeyError, IndexError):
+                # cases: image doesn't have getexif
+                pass
+
+            output = BytesIO()
+            img.save(output, format='JPEG')
+            output.seek(0)
+            self.avatar = InMemoryUploadedFile(
+                output,
+                'ImageField',
+                f'{self.avatar.name.split(".")[0]}.jpg',
+                'image/jpeg',
+                sys.getsizeof(output),
+                None
+            )
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"User {self.username}"
+
 
 class Post(models.Model):
     user = models.ForeignKey(
@@ -61,5 +96,6 @@ class Post(models.Model):
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
 
-    def __str(self):
-        return f"Post by {self.user} at {self.timestamp}"
+    def __str__(self):
+        post = (self.text[:50] + '..') if len(self.text) > 50 else self.text
+        return f"{self.user.username} at {self.timestamp}: {post}"
