1f94e973671e55d1b4626a7926c475a4d7c060dd
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-07-05 11:52:22 +0200

    pagination and likes

--- network/urls.py
+++ network/urls.py
@@ -5,11 +5,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # View Routes
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("user/<str:username>", views.user, name="user")
+
+    # API Routes
 ] 
 
 if settings.DEBUG:
--- network/views.py
+++ network/views.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
@@ -9,6 +10,17 @@ from django.urls import reverse
 from .models import User, Post
 from .utils import get_user_from_username
 
+
+# +-----------------------------------------+
+# |           GLOBAL VARIABLES              |
+# +-----------------------------------------+
+# shouild probably factor these out in a more elegant way
+PAGINATION_POST_COUNT = 10
+
+
+# +-----------------------------------------+
+# |                  FORMS                  |
+# +-----------------------------------------+
 
 class NewPostForm(forms.Form):
     post = forms.CharField(
@@ -20,11 +32,21 @@ class NewPostForm(forms.Form):
         })
     )
 
+
+# +-----------------------------------------+
+# |        VIEWS THAT RETURN PAGES          |
+# +-----------------------------------------+
+
 def index(request):
-    return render(request, "network/index.html")
+    """RETURNS THE INDEX PAGE"""
+    posts = paginated_posts(request)
+    return render(request, "network/index.html", {
+        "posts": posts
+    })
 
 
 def login_view(request):
+    """RETURNS THE LOGIN VIEW"""
     if request.method == "POST":
 
         # Attempt to sign user in
@@ -78,6 +100,42 @@ def register(request):
 
 def user(request, username):
     user = get_user_from_username(username)
+    request.view_user = user
+    posts = paginated_posts(request, user)
     return render(request, "network/user.html", {
-        "user": user
+        "view_user": user,
+        "posts": posts
     })
+
+
+# +-----------------------------------------+
+# |        VIEWS THAT RETURN DATA           |
+# +-----------------------------------------+
+# factor these out to utils or some other file?
+
+# def paginated_posts(request):
+#     """RETURNS A PAGE OF POSTS FROM THE PUBLIC TIMELINE"""
+#     page = request.GET.get('page', 1)
+#     objects = Post.objects.all()
+#     p = Paginator(objects, PAGINATION_POST_COUNT)
+#     return p.page(page)
+
+
+# def paginated_posts(request, user):
+#     """RETURNS A PAGE OF POSTS FROM A USER"""
+#     page = request.GET.get('page', 1)
+#     objects = Post.objects.filter(user=user).all()
+#     p = Paginator(objects, PAGINATION_POST_COUNT)
+#     return p.page(page)
+
+def paginated_posts(request, user=None, page=1):
+    """RETURNS A PAGE OF POSTS FROM A USER"""
+    print(user)
+    print(type(user))
+    # page = request.GET.get('page', 1)
+    if user == None:
+        objects = Post.objects.all()
+    else:
+        objects = Post.objects.filter(user=user).all()
+    p = Paginator(objects, PAGINATION_POST_COUNT)
+    return p.page(page)
