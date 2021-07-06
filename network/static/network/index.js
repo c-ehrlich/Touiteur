2df504cba5094b2d3d9d550db9a62780e2469f7d
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-07-06 16:27:33 +0200

    add eventlisteners and empty functions for 'like' and 'edit' buttons

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -3,7 +3,31 @@ console.log("loading js")
 document.addEventListener('DOMContentLoaded', function() {
   // add eventlistner to sent email
   document.querySelector('#compose-form').onsubmit = send_post; 
+  document.querySelectorAll('.post-edit-button').forEach(button => {
+    button.addEventListener('click', event => {
+      post_id = button.id.split("-")[1];
+      edit_post(post_id);
+    })
+  })
+  document.querySelectorAll('.post-like-button').forEach(button => {
+    button.addEventListener('click', event => {
+      post_id = button.id.split("-")[1];
+      like_post(post_id);
+    })
+  })
 });
+
+
+// Edits a post
+function edit_post(post_id) {
+  console.log(`editing post ${post_id}`);
+}
+
+
+// Likes a post
+function like_post(post_id) {
+  console.log(`liking post ${post_id}`);
+}
 
 
 // Sends a post
