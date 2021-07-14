63a0f7119b3c02d2072623acf521faca2d390e76
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-07-14 21:16:33 +0200

    create dynamic avatar picker that updates live

--- network/forms.py
+++ network/forms.py
@@ -114,7 +114,7 @@ class RegisterAccountStage2Form(forms.ModelForm):
         widget = forms.TextInput(attrs={
             'class': 'form-control form-control-left-padding-for-at-sign',
         }))
-    
+
     avatar = forms.ImageField(
         label = "Avatar",
         required = False,
@@ -134,45 +134,3 @@ class RegisterAccountStage2Form(forms.ModelForm):
         model = User
         fields = ['username', 'email', 'bio', 'avatar']
         # exclude = ['displayname', 'email', 'password', 'confirmation']
-
-    
-
-
-# TODO this is some stuff i copied from stackoverflow
-# see what this does and if i can use it to better implement avatars
-# class UserProfileForm(forms.ModelForm):
-#     class Meta:
-#         model = User
-
-#     def clean_avatar(self):
-#         avatar = self.cleaned_data['avatar']
-
-#         try:
-#             w, h = get_image_dimensions(avatar)
-
-#             #validate dimensions
-#             max_width = max_height = 100
-#             if w > max_width or h > max_height:
-#                 raise forms.ValidationError(
-#                     u'Please use an image that is '
-#                      '%s x %s pixels or smaller.' % (max_width, max_height))
-
-#             #validate content type
-#             main, sub = avatar.content_type.split('/')
-#             if not (main == 'image' and sub in ['jpeg', 'pjpeg', 'gif', 'png']):
-#                 raise forms.ValidationError(u'Please use a JPEG, '
-#                     'GIF or PNG image.')
-
-#             #validate file size
-#             if len(avatar) > (20 * 1024):
-#                 raise forms.ValidationError(
-#                     u'Avatar file size may not exceed 20k.')
-
-#         except AttributeError:
-#             """
-#             Handles case when we are updating the user profile
-#             and do not supply a new avatar
-#             """
-#             pass
-
-#         return avatar
---
+++ network/static/network/image_uploads.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // check if an #id-avatar element exists
+    if (document.getElementById('id_avatar')) {
+      console.log("found it!");
+    }
+
+    if (document.querySelector('#id_avatar') !== null) {
+
+      document.querySelector('#id_avatar').addEventListener('click', event => {
+        document.querySelector('#id_avatar').value = null;
+        console.log("set #id_avatar to null");
+      });
+
+      // when a file is uploaded to the #id-avatar input
+      document.querySelector('#id_avatar').addEventListener('change', function(event) {
+        // create a temp path to the file, and then put that file in #profile-pic-preview-img
+        const file = event.target.files[0];
+        const reader = new FileReader();
+        reader.onload = () => {
+            document.querySelector('#profile-pic-preview-img').src = reader.result;
+        }
+        reader.readAsDataURL(file);
+
+        // set the element '#custom-file-upload-helper-text' to display: block
+        document.querySelector('#custom-file-upload-helper-text').style.display = 'block';
+    });
+  }
+});
+
+console.log("loaded image_uploads.js");
