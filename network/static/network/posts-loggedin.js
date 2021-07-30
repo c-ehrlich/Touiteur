151f8ec403497f6ebd88760698035b57ebfa8839
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-07-30 20:35:25 +0200

    [replies] add popup notification when successfully replying

--- network/static/network/posts-loggedin.js
+++ network/static/network/posts-loggedin.js
@@ -84,6 +84,23 @@ function sendReply(postId) {
       } else if (replyCount > 1) {
         document.querySelector(`#prl-${postId}`).innerHTML = `${replyCount} replies. Click to view.`; 
       }
+      new Notify({
+        status: 'success',
+        // title: 'Notify Title',
+        text: 'Reply successfully posted.',
+        effect: 'fade',
+        speed: 300,
+        customClass: null,
+        customIcon: null,
+        showIcon: true,
+        showCloseButton: true,
+        autoclose: true,
+        autotimeout: 3000,
+        gap: 20,
+        distance: 20,
+        type: 1,
+        position: 'x-center top'
+      })
     } else {
       location.reload();
     }
