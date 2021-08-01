72b564784327b4206bf161d617d841d7091eb1f0
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-08-01 18:33:00 +0200

    [image uploads] create javascript for image upload previews in compose.html, and styling for the upload and preview

--- network/static/network/image_uploads_compose.js
+++ network/static/network/image_uploads_compose.js
@@ -1,30 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // check if an #id-avatar element exists
-    if (document.getElementById('post-image-upload')) {
-      console.log("found it!");
-    }
+  if (document.querySelector('#compose-pic-preview-img') !== null) {
+    const uploadDiv = document.querySelector('#compose-form-image');
 
-    if (document.querySelector('#post-image-upload') !== null) {
+    uploadDiv.addEventListener('click', event => {
+      uploadDiv.value = null;
+    });
 
-      document.querySelector('#id_avatar').addEventListener('click', event => {
-        document.querySelector('#id_avatar').value = null;
-        console.log("set #id_avatar to null");
-      });
-
-      // when a file is uploaded to the #id-avatar input
-      document.querySelector('#id_avatar').addEventListener('change', function(event) {
-        // create a temp path to the file, and then put that file in #profile-pic-preview-img
-        const file = event.target.files[0];
-        const reader = new FileReader();
-        reader.onload = () => {
-            document.querySelector('#profile-pic-preview-img').src = reader.result;
-        }
-        reader.readAsDataURL(file);
-
-        // set the element '#custom-file-upload-helper-text' to display: block
-        document.querySelector('#custom-file-upload-helper-text').style.display = 'block';
+    // when a file is uploaded to the #id-avatar input
+    uploadDiv.addEventListener('change', function(event) {
+      // create a temp path to the file, and then put that file in #profile-pic-preview-img
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        document.querySelector('#compose-pic-preview-img').src = reader.result;
+        document.querySelector('#compose-pic-preview-img').removeAttribute('hidden');
+      }
+      reader.readAsDataURL(file);
     });
   }
 });
 
-console.log("loaded image_uploads.js");
+console.log("loaded image_uploads_compose.js");
